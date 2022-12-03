089cfb33cb8b5050b397bd09e9085303e1a70e7b
sminars <samantha_minars@brown.edu>
2022-12-03 18:56:33 -0500

    google login button logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import GraphVis from './GraphVis'
 import logo from './logo.svg';
 import './styles/App.css';
 import * as d3 from 'd3';
+import {signInWithGoogle} from './GoogleLogin'
+
 
 function App() {
   return (
     <div className="App">
       <p className="App-header">
-        Welcome to our super cool terminal app!
+      <button className="google-button" onClick = {signInWithGoogle}>Sign in With Google</button>
+        TunedIn
       </p>
+      
       <GraphVis />
       {/* <Terminal /> */}
     </div>
