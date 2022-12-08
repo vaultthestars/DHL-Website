ba4e40aa5bd3d6988268e45baa0b78084cf65143
ddtamesis <denise_danielle_tamesis@brown.edu>
2022-12-07 19:44:13 -0500

    chore: move helper into handler

--- backend/src/main/java/server/handlers/LoadSongFeaturesHandler.java
+++ backend/src/main/java/server/handlers/LoadSongFeaturesHandler.java
@@ -2,16 +2,24 @@ package server.handlers;
 
 import com.squareup.moshi.Moshi;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.model_objects.miscellaneous.CurrentlyPlaying;
+import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
 import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
+import se.michaelthelin.spotify.model_objects.specification.Track;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRefreshRequest;
+import se.michaelthelin.spotify.requests.data.player.GetUsersCurrentlyPlayingTrackRequest;
 import se.michaelthelin.spotify.requests.data.tracks.GetAudioFeaturesForTrackRequest;
+import se.michaelthelin.spotify.requests.data.tracks.GetTrackRequest;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import user.Song;
 import user.UserDatabase;
 
 public class LoadSongFeaturesHandler implements Route {
@@ -30,8 +38,6 @@ public class LoadSongFeaturesHandler implements Route {
     // write helper that takes access token and returns song object with title, artist, id &
     // features of current song
     // update song field of user object to contain new song object
-
-
 
     String id = "4ewazQLXFTDC8XvCbhvtXs"; // Glimpse of us by Joji (mock song ID)
 
@@ -97,9 +103,53 @@ public class LoadSongFeaturesHandler implements Route {
     } catch (IOException | SpotifyWebApiException | ParseException e) {
       System.out.println("Error: " + e.getMessage());
       throw new RuntimeException(e);
-
     }
+  }
 
+  /**
+   * Helper that gets the current song of a user and returns a Song object with relevant info.
+   *
+   * @param username - the username of the user whose song to get
+   * @param accessToken - the access token for the user
+   * @return their current song as a Song object
+   */
+  public Song getCurrentSong(String username, String accessToken) {
+    SpotifyApi spotifyApi = new SpotifyApi.Builder()
+        .setAccessToken(accessToken)
+        .build();
+    GetUsersCurrentlyPlayingTrackRequest getUsersCurrentlyPlayingTrackRequest = spotifyApi
+        .getUsersCurrentlyPlayingTrack()
+        .build();
+    try {
+      CurrentlyPlaying currentlyPlaying = getUsersCurrentlyPlayingTrackRequest.execute();
+      String title = currentlyPlaying.getItem().getName();
+      String id = currentlyPlaying.getItem().getId();
+
+      List<String> artists = new ArrayList<>();
+      GetTrackRequest getTrackRequest = spotifyApi.getTrack(id)
+          .build();
+      Track track = getTrackRequest.execute();
+      ArtistSimplified[] artistsSimp = track.getArtists();
+      for (ArtistSimplified artist : artistsSimp) {
+        artists.add(artist.getName());
+      }
+
+      GetAudioFeaturesForTrackRequest getAudioFeaturesForTrackRequest =
+          spotifyApi.getAudioFeaturesForTrack(id).build();
+      AudioFeatures audioFeatures = getAudioFeaturesForTrackRequest.execute();
+
+      float[] features = new float[6];
+      features[0] = audioFeatures.getAcousticness();
+      features[1] = audioFeatures.getDanceability();
+      features[2] = audioFeatures.getEnergy();
+      features[3] = audioFeatures.getInstrumentalness();
+      features[4] = audioFeatures.getSpeechiness();
+      features[5] = audioFeatures.getValence();
+
+      return new Song(title, id, artists, features, username);
+    } catch (IOException | SpotifyWebApiException | ParseException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
 
