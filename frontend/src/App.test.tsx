ffd180e5bed590a539cb820b4404f641ed1764cd
ceboop <84917913+ceboop@users.noreply.github.com>
2022-12-21 07:44:44 +0900

    Update App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,10 +3,9 @@ import { render, screen } from '@testing-library/react';
 import App, { initdist } from './App';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
-import {genrandomstring, mag, sech, radsort, linsort, getdata, getdatastrings, getdatamatches, repulse, towardsort, sortshift, updatecamcenter, getsortname, getparamname} from './GraphVis';
+import GraphVis, {genrandomstring, mag, sech, radsort, linsort, getdata, getdatastrings, getdatamatches, repulse, towardsort, sortshift, updatecamcenter, getsortname, getparamname} from './GraphVis';
 
 beforeEach(() => {
-    render(<App />);
 });
 
 afterEach(() => {
@@ -15,18 +14,55 @@ afterEach(() => {
 //testing based on the idea of 'tests for this front-end application should be written in terms of what the end-user can perceive'
 
 test('renders app header', () => {
-    const headerElement = screen.getByLabelText("App-header")
+    render(<App />);
+    const headerElement = screen.getByLabelText("App header")
     expect(headerElement).toBeInTheDocument();
 });
 
 test('renders sign in box', () => {
-    const loginElement = screen.getByLabelText("sign-into-google")
+    render(<App />);
+    const loginElement = screen.getByLabelText("Click here to sign in with google")
     expect(loginElement).toBeInTheDocument();
 });
 
-test('renders sign in box', () => {
-    const loginElement = screen.getByLabelText("spotify-button")
+test('renders logo', () => {
+    render(<App />);
+    const logoElement = screen.getByLabelText("Logo for the tunedin website")
+    expect(logoElement).toBeInTheDocument();
+});
+
+test('renders spotify button', () => {
+    render(<App />);
+    const loginElement = screen.getByLabelText("Click here to log in to spotify")
     expect(loginElement).toBeInTheDocument();
+});
+
+test('render graphviz and test stuff', () => {
+    render(GraphVis(
+        "uid2", //CurrentGoogleUser
+        true, //spotifyLinked
+        true, //usersloaded
+        false, //fetchingusers
+        new Map([[0, [1, 1, 1, 1, 1, 1]], [1, [1, 1, 1, 1, 1, 1]]]), //usersongparams
+        new Map([[0, ["songName", "songArtist"]], [1, ["songName2", "songArtist2"]]]), //userdatastrings, 
+        new Map([[1, [[0], [0]]], [0, [[1], [1]]]]), //matchesdata,
+        .5, //Timer
+        ["uid1", "uid2"], //userIDs
+        [[0, 1, 2], [1, 2, 3]], //CircleData [us]
+        1, //SortParameter for default
+        1, //SortIndex for sort style
+        [1, 0, 0], //camcenter, 
+        1, //SelectIndex, 
+        0, //zoomval, 
+        false, //zoomed, 
+        false, //alltime, 
+        0, //curruserindex,
+        (bool: boolean) => {}, //Setalltime, 
+        (num: number) => {}, //setSelectIndex, 
+        (bool: boolean) => {}, //setZoomed, 
+        (num: number) => {}, //setSortParameter, 
+        (num: number) => {}, //setSortIndex
+        ));
 });
 
 test('log in logic', () => {
@@ -74,13 +110,13 @@ test('radsort()', () => {
 
 
 
-test('repulse()', () => {
-    const result: Array<number> = repulse([1, 2, 3], [2, 2, 6])
-    expect(result.length).toBe(2)
-    expect(result[0]).is[0]
-    expect(result[1] < -0.99 && result[1]>-1).toBe(true)
-    // should only repulse in the y direction if they are in the same x position
-});
+// test('repulse()', () => {
+//     const result: Array<number> = repulse([1, 2, 3], [2, 2, 6])
+//     expect(result.length).toBe(2)
+//     expect(result[0]).is[0]
+//     expect(result[1] < -0.99 && result[1]>-1).toBe(true)
+//     // should only repulse in the y direction if they are in the same x position
+// });
 
 test('towardsort()', () => {
 });
