90be6a3ca105cd0ec9b1c7b591ba8c3356b0f612
sminars <samantha_minars@brown.edu>
2022-12-20 13:51:48 -0500

    writing comments

--- backend/src/main/java/server/handlers/GetUserHandler.java
+++ backend/src/main/java/server/handlers/GetUserHandler.java
@@ -9,26 +9,43 @@ import spark.Response;
 import spark.Route;
 import user.User;
 
+/**
+ * Handler class for the get-user endpoint
+ */
 public class GetUserHandler implements Route {
 
   UserDatabase database;
 
+  /**
+   * Constructor
+   * @param database that stores users
+   */
   public GetUserHandler(UserDatabase database) {
     this.database = database;
   }
 
+  /**
+   * Retrieves user from database using the request query parameter id
+   * @param request
+   * @param response
+   * @return a serialized success or error response
+   */
   @Override
   public Object handle(Request request, Response response) {
     try {
       String userId = request.queryParams("id");
       User user = this.database.getUser(userId);
-      System.out.println("User retrieved.");
       return new GetUserSuccessResponse(user).serialize();
     } catch (Exception e) {
       return new ErrBadRequestResponse().serialize();
     }
   }
 
+  /**
+   * Response object to send with User object
+   * @param result - success message
+   * @param user - User object that was retrieved from the database
+   */
   public record GetUserSuccessResponse(String result, User user) {
 
     public GetUserSuccessResponse(User user) {
