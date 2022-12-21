e37fe60c01517cc49d446474c22741a347ab9083
ceboop <84917913+ceboop@users.noreply.github.com>
2022-12-21 11:04:17 +0900

    frontend testing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,7 +3,7 @@ import { cleanup, render, screen } from '@testing-library/react';
 import App, { initdist } from './App';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
-import GraphVis, {genrandomstring, mag, sech, radsort, linsort, getdata, getdatastrings, getdatamatches, repulse, towardsort, sortshift, updatecamcenter, getsortname, getparamname} from './GraphVis';
+import GraphVis, {dots, fullyloggedin, textbuff, genrandomstring, mag, sech, radsort, linsort, getdata, getdatastrings, getdatamatches, repulse, towardsort, sortshift, updatecamcenter, getsortname, getparamname} from './GraphVis';
 
 const genericGraphviz = GraphVis(
     "uid2", //CurrentGoogleUser
@@ -64,10 +64,48 @@ test('renders spotify button', () => {
     expect(loginElement).toBeInTheDocument();
 });
 
-test('render graphviz and test stuff', () => {
+test('render loading screen in graphviz if fetching users', () => {
     render(GraphVis(
         "uid2", //CurrentGoogleUser
         true, //spotifyLinked
+        false, //usersloaded
+        true, //fetchingusers
+        new Map([[0, [1, 1, 1, 1, 1, 1]], [1, [1, 1, 1, 1, 1, 1]]]), //usersongparams
+        new Map([[0, ["songName", "songArtist"]], [1, ["songName2", "songArtist2"]]]), //userdatastrings, 
+        new Map([[1, [[0], [0]]], [0, [[1], [1]]]]), //matchesdata,
+        .5, //Timer
+        ["uid1", "uid2"], //userIDs
+        [[0, 1, 2], [1, 2, 3]], //CircleData [us]
+        1, //SortParameter for default
+        1, //SortIndex for sort style
+        [1, 0, 0], //camcenter, 
+        1, //SelectIndex, 
+        0, //zoomval, 
+        false, //zoomed, 
+        false, //alltime, 
+        0, //curruserindex,
+        (bool: boolean) => {}, //Setalltime, 
+        (num: number) => {}, //setSelectIndex, 
+        (bool: boolean) => {}, //setZoomed, 
+        (num: number) => {}, //setSortParameter, 
+        (num: number) => {}, //setSortIndex
+        ));
+        const loadingElement = screen.getByLabelText("loading screen")
+        expect(loadingElement).toBeInTheDocument();
+
+        let errorMsg;
+        try {
+            const notElement = screen.getByLabelText("user sidebar for displaying your current matches")
+        } catch {
+            errorMsg = "error thrown"
+        }
+        expect(errorMsg).toBe("error thrown");
+});
+
+test('render nno sidebar if spotify is not linked', () => {
+    render(GraphVis(
+        "uid2", //CurrentGoogleUser
+        false, //spotifyLinked
         true, //usersloaded
         false, //fetchingusers
         new Map([[0, [1, 1, 1, 1, 1, 1]], [1, [1, 1, 1, 1, 1, 1]]]), //usersongparams
@@ -90,12 +128,36 @@ test('render graphviz and test stuff', () => {
         (num: number) => {}, //setSortParameter, 
         (num: number) => {}, //setSortIndex
         ));
+        const sidebar = screen.getByLabelText("hidden")
+        expect(sidebar).toBeInTheDocument();
 });
 
-test('log in logic', () => {
-    
+test('render nno sidebar if spotify is not linked', () => {
+    render(genericGraphviz)
+    const sidebar = screen.getByLabelText("sidebar")
+    expect(sidebar).toBeInTheDocument();
+    const matches = screen.getByLabelText("click here to view the top 5 users you match with of all time below")
+    expect(matches).toBeInTheDocument();
 });
+
 // TESTS FOR FUNCTIONS IN GRAPHVIZ
+test('textbuff()', () => {
+    const result = textbuff("hiiii", 2)
+    expect(result).toBe("hi...")
+});
+
+test('dots()', () => {
+    const result = dots(1)
+    expect(result).toBe("...")
+});
+
+test('fullyloggedin()', () => {
+    const result = fullyloggedin(true, "className")
+    expect(result).toBe("className")
+    const result1 = fullyloggedin(false, "className")
+    expect(result1).toBe("hidden")
+});
+
 test('genradnomstring()', () => {
     const result = genrandomstring(4)
     expect(result.length).toBe(4)
@@ -203,6 +265,4 @@ test('sortShift()', () => {
 test('updatecamcenter()', () => {
     const result1 = updatecamcenter([0, 0, 0], [1, 1, 1])
     expect([Math.floor(result1[0]*100), Math.floor(result1[1]*100), Math.floor(result1[2]*100)]).toStrictEqual([3, 3, 3])
-
 });
-
