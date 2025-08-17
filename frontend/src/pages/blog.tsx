09908f4a9da651426bb42a69b9f72045ffbec733
vaultthestars <94648408+vaultthestars@users.noreply.github.com>
2025-08-17 01:49:13 -0700

    blog updates, cursed ideas and evil will ferrell

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -15,6 +15,42 @@ export const blogtext =
 <br/>
 <br/>
 
+
+8/17/25
+<br/>
+
+A man who looked like an evil Will Ferrell just walked past Pike Place Market. I turned to my sister and she had the exact same
+thought, which makes me wonder if we really did see Will Ferrell and he was just having a particularly terrible day.
+
+<br/>
+<br/>
+
+8/10/25
+<br/>
+AN UNORDERED COLLECTION OF CURSED IDEAS I HAVE ACCUMULATED IN MY NOTES APP:
+<ul>
+    <li>A hat with ear holes</li>
+    <li>A pie with either a bell pepper or an entire slice of charred and incinerated toast inside</li>
+    <li>Meat flavored hint water</li>
+    <li>Sticking as many phone holders as possible on a car and using them as a widescreen display</li>
+    <li>A trombone with two degrees of freedom</li>
+    <li>Getting your face tattooed on your face, but slightly smaller</li>
+    <li>Keeping your bangs long and then cutting eyeholes in them</li>
+    <li>Dyeing your hair either translucent, vantablack[the darkest chemically synthesized paint on earth], or skin tone</li>
+    <li>A smell every smell in the world Guinness World Record speedrun category</li>
+    <li>Carbonated clam chowder</li>
+    <li>Homing smells- smells that once opened, pathfind their way to a target</li>
+    <li>A fancy restaurant where they give you a single chopstick to eat soup with</li>
+    <li>Slicing a potato by smashing it through the strings of a guitar</li>
+    <li>Inflatable dobok that turns into sparring gear</li>
+    <li>Hinged hard shell taco</li>
+    <li>A pasta shape that is just a massive flaccid wheel with a million spokes so you think you’re eating a bowl of pasta but it’s just an absurdly long single pasta network</li>
+</ul>
+
+
+<br/>
+<br/>
+
 8/6/25
 <br/>
 I've been thinking about expanding my culinary palette and getting into slurries and gruels.
@@ -25,21 +61,35 @@ I've been thinking about expanding my culinary palette and getting into slurries
 <br/>
 <b>ROOMMATE AGREEMENT:</b>
 <br/>
-<br/>
 About a year ago, I moved into a new dorm suite with a few of my friends and we had to fill out a series of questions that 
 would lay down the ground rules for how we should live and interact with each other. Here is what we ended up settling on:
 <br/>
 {/* <img width = "800" src = {room1}/> */}
-<img width = "800" src = {room2}/>
-<img width = "800" src = {room3}/>
-<img width = "800" src = {room4}/>
-<img width = "800" src = {room5}/>
-<img width = "800" src = {room6}/>
-<img width = "800" src = {room7}/>
-<img width = "800" src = {room8}/>
+<img width = "600" src = {room2}/>
+<br/>
+<img width = "600" src = {room3}/>
+<br/>
+<img width = "600" src = {room4}/>
+<br/>
+<img width = "600" src = {room5}/>
+<br/>
+<img width = "600" src = {room6}/>
+<br/>
+<img width = "600" src = {room7}/>
+<br/>
+<img width = "600" src = {room8}/>
 
 <br/>
 <br/>
+
+8/5/25
+
+<br/>
+<br/>
+
+We went to the Mattress Firm the other day to look at mattresses and we had the most incredible mattress salesman.
+Every time he introduced a new mattress to us he'd walk over to the mattress and slap it like a car salesman would slap
+a car, and say something like "This tempurpedic mattress has patented cooling technology." Best hour and a half of my life.
 
 <br/>
 
